3424540432146c0f02b69fe094d02ce14b63e8a1
Winterbox400 <sandigojustin@gmail.com>
2025-03-18 15:17:54 -0600

    DAO/Connection: Crear Conexiono a la base de datos

---
+++ DAO/Connection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace DAO
+{
+    public class Connection
+    {
+        private readonly static string Server = "WINTERBOX400\\SQL2022";
+        private readonly static string Database = "BD_ControlBiblioteca";
+        private readonly static string User = "sa";
+        private readonly static string Password = "1234";
+        private readonly string connectionString = $"Server={Server};Database={Database};User Id={User};Password={Password};";
+
+        public async Task<SqlConnection> GetConexionAsync()
+        {
+            try
+            {
+                var connection = new SqlConnection(connectionString);
+                await connection.OpenAsync();
+                return connection;
+            } catch (SqlException ex)
+            {
+                Console.WriteLine($"Hubo un error al conectarse a la base de datos\n");
+                Console.WriteLine($"Codigo de error: {ex.ErrorCode}");
+                return null;
+            }
+        }
+    }
+}
