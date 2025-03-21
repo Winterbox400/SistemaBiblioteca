92d2d1cc9029801c250124475bef937b4a1662f4
Winterbox400 <sandigojustin@gmail.com>
2025-03-21 02:18:48 -0600

    GUI/Forms => Creación y diseño Login; GUI/Resources => Assets para el Login

--- DAO/obj/Debug/net8.0/DAO.AssemblyInfo.cs
+++ DAO/obj/Debug/net8.0/DAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7874f91934e5d4ce754a9b3735a86d2f628bfff3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
